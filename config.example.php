e6e8305830aa6f893d95aee1686458959db06a8f
Repflez <Repflez@users.noreply.github.com>
2018-11-29 13:45:29 -0700

    Update base config with current plans for the future

--- config.example.php
+++ config.example.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * foxverse Configuration File.
+ * rverse2 Configuration File.
  */
 return [
     // Database configuration according to https://laravel.com/docs/5.2/database#introduction
@@ -21,13 +21,13 @@ return [
     // General site settings
     'general'       => [
         // Name of the site
-        'name'              => 'foxverse',
+        'name'              => 'rverse2',
 
         // Default logo of the site (empty for the default)
         'logo'              => '',
 
         // Description of the site
-        'description'       => 'foxverse is a Miiverse clone for 3DS, Wii U and PC!',
+        'description'       => 'rverse is a Miiverse clone for 3DS, Wii U and PC!',
 
         // Close the site for maintenance
         'maintenance'       => false,
@@ -37,6 +37,9 @@ return [
 
         // Hashids salt (prevents stuff from getting scraped easily)
         'link_salt'         => 'changethis',
+
+        // Allowed domains
+        'domains'     => ['rverse.club'],
     ],
 
     // Cloudinary settings
@@ -71,7 +74,7 @@ return [
 
     // Cookie settings
     'cookie'        => [
-        'prefix'            => 'miiverse_',
+        'prefix'            => 'rverse_',
     ],
 
     // Performance settings
@@ -94,20 +97,74 @@ return [
         // Enable twig (the templating engine) debug mode
         'twig_debug'        => false,
 
-        // Host for the shyla dev:serve command
+        // Host for the topia dev:serve command
         'host'              => 'localhost:8000',
 
         // Reporting host address
         'report_host'       => '',
     ],
 
+    // User settings
+    'user'     => [
+        // Minimum entropy value a password needs to have
+        'pass_min_entropy'  => 1,
+
+        // Username constraints
+        'name_min'          => 3,
+        'name_max'          => 16,
+
+        // Disable registration, just in case
+        'disable_signups'   => true,
+
+        // Require the user to click a link in an e-mail sent to them
+        'require_activation'=> true,
+
+        // How long a username should be reserved in days
+        'name_reserve'      => 90,
+
+        // How long a user should be inactive till another person can use their name
+        'name_takeover'     => 365,
+    ],
+
+    // Donation settings
+    'dontation'     => [
+        'max_monts'         => 24,
+        'price_per_month'   => 5.00,
+        'price_unit'        => 'USD',
+    ],
+
+    // PayPal settings
+    'paypal'        => [
+        'mode'              => 'sandbox',
+        'client_id'         => '',
+        'secret_id'         => '',
+    ],
+
     // Ranks ids, these ranks are used by automated procedures in the backend
     // If you're using the setup command in topia, these are already set correctly for you!
     'rank'          => [
+        'banned'            => -1,
+        'guest'             => 0,
         'regular'           => 1,
         'verified'          => 2,
-        'mod'               => 3,
-        'admin'             => 4,
-        'banned'            => 5,
+        'bot'               => 3,
+        'donator'           => 4,
+        'alumni'            => 5,
+        'mod'               => 6,
+        'admin'             => 7,
+    ],
+
+    // Comment settings
+    'comments'      => [
+        'max_length'        => 5000,
+        'min_length'        => 1,
+        'daily_limit'       => 30,
+    ],
+
+    // Separate websites with its own routes
+    'sites' => [
+        '3ds'               => '',
+        'wiiu'              => '',
+        'others'            => '',
     ],
 ];
