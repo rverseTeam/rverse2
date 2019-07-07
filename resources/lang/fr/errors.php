02198371327731454b04e75724d8df5e5118b87e
mrbob312 <mrbob312@users.noreply.github.com>
2019-07-07 11:32:30 +0200

    Added French translation

---
+++ resources/lang/fr/errors.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Translation file for errors
+ */
+
+return [
+    '403' => [
+        'title' => 'Erreur',
+        'desc' => 'L\'écran n\'a pas pu être affiché.',
+    ],
+
+    '404' => [
+        'title' => 'Erreur',
+        'desc' => 'L\'écran n\'a pas pu être affiché.',
+    ],
+];
---
+++ resources/lang/fr/gate.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Translation file for gate
+ */
+
+return [
+    'welcome' => [
+        'controls' => [
+            'back' => 'Retour',
+            'close' => 'Fermer',
+            'next' => 'Suivant',
+        ],
+        'intro' => [
+            'title' => 'Bienvenue sur :name!',
+            'message' => ':name vous permet de partager vos expériences de jeu avec des joueurs du monde entier.',
+        ],
+        'about' => [
+            'title' => 'À propos du :name',
+            'message1' => ':name est une reconstitution de Miiverse faite par des fans pour des fans.',
+            'message2' => 'Comme il s’agit d’une récréation, il n’aura peut-être pas les mêmes caractéristiques que le défunt Miiverse et nous travaillons à les rendre disponibles le plus rapidement possible.',
+        ],
+        'config' => [
+            'title' => 'Configurer votre compte :name',
+            'message' => 'Nous avons besoin de vos données avant de pouvoir commencer à utiliser :name.',
+            'display_name' => 'Nom affiché',
+            'nnid' => 'Nintendo Network ID',
+            'nnid_label' => 'Nous utilisons votre identifiant Nintendo Network pour récupérer votre image Mii.',
+        ],
+        'finish' => [
+            'title' => 'Amusez-vous sur :name!',
+            'start' => 'Début',
+        ],
+    ],
+
+    'welcome_guest' => [
+        'title' => ':name est une communauté de jeux qui met en relation des personnes du monde entier utilisant des personnages Mii.<br>Utilisez :name pour partager vos expériences de jeu et rencontrer des gens du monde entier.',
+        'message' => 'Vous aurez besoin d\'un Nintendo ID réseau pour créer des publications ou créer un profil. Sélectionnez ID lien pour lier un ID nouveau ou existant.',
+        'link' => 'Lien ID',
+        'exit' => 'Sortie',
+    ],
+];
