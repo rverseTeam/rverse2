0f84b2ad99f40f23faff2d35b3039856971a549e
mrbob312 <mrbob312@users.noreply.github.com>
2019-07-07 12:11:18 +0200

    Added German translation

---
+++ resources/lang/de/community.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Translation file for community
+ */
+
+return [
+    'index' => [
+        'header' => [
+            'desc' => 'Besuchen Sie in die Gemeinschaften für Spielen, die Sie spielen, oder für Spielen, auf die Sie neugierig sind!',
+            'title' => 'Gemeinschaften',
+        ],
+        'favorites' => 'Favorite Gemeinschaften',
+        'search' => [
+            'desc' => 'Suche Gemeinschaften',
+            'name' => 'Suchen',
+        ],
+        'communities' => [
+            'filter' => [
+                'name' => 'Filter',
+                'select' => 'Bitte triff eine Auswahl.',
+                'options' => [
+                    'all' => 'Alle Software',
+                    'games' => ':console-spiele',
+                    'vc' => 'Virtuelle Konsole',
+                    'other' => 'Andere',
+                ],
+            ],
+            'new' => 'Neue Gemeinschaften',
+            'special' => 'Besondere',
+            'platforms' => [
+                '3ds' => '3DS-Spiele',
+                'wiiu' => 'Wii U-Spiele',
+                'wiiu-3ds' => 'Wii U-Spiele・3DS-Spiele',
+                'switch' => 'Switch-Spiele',
+                'switch-wiiu' => 'Switch-Spiele・Wii U-Spiele',
+                'wii' => 'Wii-Spiele',
+                'ds' => 'DS-Spiele',
+                'wii-ds' => 'Wii-Spiele・DS-Spiele',
+                'special' => 'Besondere Gemeinschaft',
+            ],
+            'empty' => 'In dieser Kategorie gibt es keine Gemeinschaften.',
+            'more' => 'Zeig mehr',
+        ],
+        'footer' => [
+            'console' => ':console-Gemeinschaften',
+        ],
+    ],
+
+    'listing' => [
+        'filter' => [
+            'name' => 'Filter',
+            'select' => 'Bitte triff eine Auswahl.',
+            'options' => [
+                'all' => 'Alle Software',
+                'games' => ':console-Spiele',
+                'vc' => 'Virtuelle Konsole',
+                'other' => 'Andere',
+            ],
+        ],
+        'communities' => [
+            'platforms' => [
+                '3ds' => '3DS-Spiele',
+                'wiiu' => 'Wii U-Spiele',
+                'wiiu-3ds' => 'Wii U-Spiele・3DS-Spiele',
+                'switch' => 'Switch-Spiele',
+                'switch-wiiu' => 'Switch-Spiele・Wii U-Spiele',
+                'wii' => 'Wii-Spiele',
+                'ds' => 'DS-Spiele',
+                'wii-ds' => 'Wii-Spiele・DS-Spiele',
+                'special' => 'Besondere Gemeinschaft',
+            ],
+            'empty' => 'In dieser Kategorie gibt es keine Gemeinschaften.',
+        ],
+    ],
+];
