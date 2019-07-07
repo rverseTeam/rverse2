0f5f3190167c9bbc51db0379a119aee51ae692c9
mrbob312 <mrbob312@users.noreply.github.com>
2019-07-07 10:56:50 +0200

    Added Dutch translation

---
+++ resources/lang/nl/community.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Translation file for community
+ */
+
+return [
+    'index' => [
+        'header' => [
+            'desc' => 'Bekijk de communities voor games die je speelt of games waar je nieuwsgierig naar bent!',
+            'title' => 'Communities',
+        ],
+        'favorites' => 'Favoriete Communities',
+        'search' => [
+            'desc' => 'Zoek naar Communities',
+            'name' => 'Zoeken',
+        ],
+        'communities' => [
+            'filter' => [
+                'name' => 'Filter',
+                'select' => 'Maak A.U.B. een selectie.',
+                'options' => [
+                    'all' => 'Alle Software',
+                    'games' => ':console Games',
+                    'vc' => 'Virtuele Console',
+                    'other' => 'Anders',
+                ],
+            ],
+            'new' => 'Nieuwe Communities',
+            'special' => 'Speciaal',
+            'platforms' => [
+                '3ds' => '3DS Games',
+                'wiiu' => 'Wii U Games',
+                'wiiu-3ds' => 'Wii U Games・3DS Games',
+                'switch' => 'Switch Games',
+                'switch-wiiu' => 'Switch Games・Wii U Games',
+                'wii' => 'Wii Games',
+                'ds' => 'DS Games',
+                'wii-ds' => 'Wii Games・DS Games',
+                'special' => 'Speciale Community',
+            ],
+            'empty' => 'Er zijn geen communities in deze categorie.',
+            'more' => 'Laat meer zien',
+        ],
+        'footer' => [
+            'console' => ':console Communities',
+        ],
+    ],
+
+    'listing' => [
+        'filter' => [
+            'name' => 'Filter',
+            'select' => 'Maak A.U.B. een selectie.',
+            'options' => [
+                'all' => 'Alle Software',
+                'games' => ':console Games',
+                'vc' => 'Virtuele Console',
+                'other' => 'Anders',
+            ],
+        ],
+        'communities' => [
+            'platforms' => [
+                '3ds' => '3DS Games',
+                'wiiu' => 'Wii U Games',
+                'wiiu-3ds' => 'Wii U Games・3DS Games',
+                'switch' => 'Switch Games',
+                'switch-wiiu' => 'Switch Games・Wii U Games',
+                'wii' => 'Wii Games',
+                'ds' => 'DS Games',
+                'wii-ds' => 'Wii Games・DS Games',
+            ],
+            'empty' => 'Er zijn geen communities in deze categorie.',
+        ],
+    ],
+];
---
+++ resources/lang/nl/errors.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Translation file for errors
+ */
+
+return [
+    '403' => [
+        'title' => 'Error',
+        'desc' => 'Het scherm kon niet worden weergegeven.',
+    ],
+
+    '404' => [
+        'title' => 'Error',
+        'desc' => 'Het scherm kon niet worden weergegeven.',
+    ],
+];
