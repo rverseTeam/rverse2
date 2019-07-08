10ca3fd65866d0bc3a4c1fbcd944c6defcde8bda
mrbob312 <mrbob312@users.noreply.github.com>
2019-07-08 13:52:44 +0200

    Improved French even more

--- resources/lang/fr/gate.php
+++ resources/lang/fr/gate.php
@@ -24,7 +24,7 @@ return [
             'message' => 'Nous avons besoin de vos données avant de pouvoir commencer à utiliser :name.',
             'display_name' => 'Nom affiché',
             'nnid' => 'ID Nintendo Network',
-            'nnid_label' => 'Nous utilisons votre ID Nintendo Network pour récupérer votre image Mii.',
+            'nnid_label' => 'Nous utilisons votre Identifiant Nintendo Network pour récupérer votre image Mii.',
         ],
         'finish' => [
             'title' => 'Amusez-vous sur :name!',
@@ -35,7 +35,7 @@ return [
     'welcome_guest' => [
         'title' => ':name est une communauté de jeux qui met en relation des personnes du monde entier utilisant des personnages Mii.<br>Utilisez :name pour partager vos expériences de jeu et rencontrer des gens du monde entier.',
         'message' => 'Vous aurez besoin d\'un Nintendo ID réseau pour créer des publications ou créer un profil. Sélectionnez ID lien pour lier un ID nouveau ou existant.',
-        'link' => 'Lien ID',
+        'link' => 'Lier l\'ID',
         'exit' => 'Sortie',
     ],
 ];
