3a1c979843481aca8092eb283757dd058b31ecfc
mrbob312 <34964788+mrbob312@users.noreply.github.com>
2020-05-04 17:37:07 +0200

    Added Norwegian translation

---
+++ resources/lang/no/gate.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Translation file for gate
+ */
+
+return [
+    'welcome' => [
+        'controls' => [
+            'back' => 'Tilbake',
+            'close' => 'Lukk',
+            'next' => 'Neste',
+        ],
+        'intro' => [
+            'title' => 'Velkommen til :name!',
+            'message' => ':name lar deg dele dine opplevelser i spillet med mennesker fra hele verden.',
+        ],
+        'about' => [
+            'title' => 'Om :name',
+            'message1' => ':name er en rekreasjon av Miiverse laget av fans for fans.',
+            'message2' => 'Siden dette er en rekreasjon, har den kanskje ikke de samme funksjonene som den nedlagte Miiverse, og vi jobber med å gjøre dem tilgjengelige så snart som mulig.',
+        ],
+        'config' => [
+            'title' => 'Konfigurere :name-kontoen din',
+            'message' => 'Vi trenger noen data fra deg før du kan begynne å bruke :name.',
+            'display_name' => 'Visningsnavn',
+            'nnid' => 'Nintendo Network ID',
+            'nnid_label' => 'Vi bruker din Nintendo Network ID for å hente ditt Mii-bilde.',
+        ],
+        'finish' => [
+            'title' => 'Ha det gøy i :name!',
+            'start' => 'Start',
+        ],
+    ],
+
+    'welcome_guest' => [
+        'title' => ':name er et spillsamfunn som kobler mennesker fra hele verden ved hjelp av Mii-tegn. <br>Bruk :name for å dele dine spillopplevelser og møte mennesker fra hele verden.',
+        'message' => 'Du trenger en Nintendo Network ID for å opprette innlegg eller opprette en profil. Velg Link ID for å koble en ny eller eksisterende ID.',
+        'link' => 'Link ID',
+        'exit' => 'Exit',
+    ],
+];
