70dc3cc46d0586f933589ae8b8935ae328974192
Bora <mac@Mac-MacBook-Air-2.local>
2020-05-04 11:19:45 +0300

    Added Turkish Translation && Added .DS_STORE to the gitignore

---
+++ resources/lang/tr/errors.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Translation file for errors
+ */
+
+return [
+    '403' => [
+        'title' => 'Hata',
+        'desc' => 'Bu ekran gösterilemiyor.',
+    ],
+
+    '404' => [
+        'title' => 'Hata',
+        'desc' => 'Bu ekran gösterilemiyor.',
+    ],
+];
