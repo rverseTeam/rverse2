b68bef68a100135f72c740191902973a22ff58c4
wuniversales <48658161+wuniversales@users.noreply.github.com>
2020-05-22 19:46:50 +0200

    Added Spanish Lang

---
+++ resources/lang/es/community.php
@@ -0,0 +1,74 @@
+ <?php
+/*
+ * Translation file for community
+ */
+
+return [
+    'index' => [
+        'header' => [
+            'desc' => 'Check out the communities for games that you play or games that you\'re curious about!',
+            'title' => 'Comunidades',
+        ],
+        'favorites' => 'Comunidades favoritas',
+        'search' => [
+            'desc' => 'Buscar comunidades',
+            'name' => 'Buscar',
+        ],
+        'communities' => [
+            'filter' => [
+                'name' => 'Filtro',
+                'select' => 'Por favor haz una selección.',
+                'options' => [
+                    'all' => 'Todo el software',
+                    'games' => 'Juegos de :console',
+                    'vc' => 'Consola Virtual',
+                    'other' => 'Otros',
+                ],
+            ],
+            'new' => 'Nuevas comunidades',
+            'special' => 'Especial',
+            'platforms' => [
+                '3ds' => 'Juegos 3DS',
+                'wiiu' => 'Juegos Wii U',
+                'wiiu-3ds' => 'Juegos Wii U ・Juegos 3DS',
+                'switch' => 'Juegos Switch',
+                'switch-wiiu' => 'Juegos Switch・Juegos Wii U',
+                'wii' => 'Juegos Wii',
+                'ds' => 'Juegos DS',
+                'wii-ds' => 'Juegos Wii・Juegos DS',
+                'special' => 'Comunidad Especial',
+            ],
+            'empty' => 'No hay comunidades en esta categoría.',
+            'more' => 'Mostrar más',
+        ],
+        'footer' => [
+            'console' => 'Comunidades  de :console',
+        ],
+    ],
+
+    'listing' => [
+        'filter' => [
+            'name' => 'Filtro',
+            'select' => 'Por favor haz una selección.',
+            'options' => [
+                    'all' => 'Todo el software',
+                    'games' => 'Juegos de :console',
+                    'vc' => 'Consola Virtual',
+                    'other' => 'Otros',
+            ],
+        ],
+        'communities' => [
+            'platforms' => [
+                '3ds' => 'Juegos 3DS',
+                'wiiu' => 'Juegos Wii U',
+                'wiiu-3ds' => 'Juegos Wii U ・Juegos 3DS',
+                'switch' => 'Juegos Switch',
+                'switch-wiiu' => 'Juegos Switch・Juegos Wii U',
+                'wii' => 'Juegos Wii',
+                'ds' => 'Juegos DS',
+                'wii-ds' => 'Juegos Wii・Juegos DS',
+            ],
+            'empty' => 'No hay comunidades en esta categoría.',
+        ],
+    ],
+];
