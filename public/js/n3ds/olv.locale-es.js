c9eb6c6cba102af5d24113560b82ab4ed517c5cc
wuniversales <48658161+wuniversales@users.noreply.github.com>
2020-05-23 14:37:13 +0200

    Added spanish language to nintendo 3ds js file

---
+++ public/js/n3ds/olv.locale-es.js
@@ -0,0 +1,441 @@
+Olv.Locale.Data = {
+    "olv.datetime": {
+        "value": "MM/dd/yyyy h:mm a"
+    },
+    "olv.datetime.n3ds.date": {
+        "args": [2, 3, 1],
+        "value": "%s/%s/%s"
+    },
+    "olv.datetime.n3ds.date %1%2%3": {
+        "args": [2, 3, 1],
+        "value": "%s/%s/%s"
+    },
+    "olv.datetime.n3ds.date [_1][_2][_3]": {
+        "args": [2, 3, 1],
+        "value": "%s/%s/%s"
+    },
+    "olv.portal.age_gate.select_label": {
+        "value": "Por favor, introduzca su fecha de nacimiento."
+    },
+    "olv.portal.album.delete_confirm": {
+        "value": "¿Estás seguro de que quieres eliminar esto?"
+    },
+    "olv.portal.album.save_album_image": {
+        "value": "Guardar captura de pantalla"
+    },
+    "olv.portal.album.save_album_image.confirm": {
+        "value": "La captura de pantalla ha sido guardada. ¿Continuar usando rverse?"
+    },
+    "olv.portal.back": {
+        "value": "Atrás"
+    },
+    "olv.portal.block": {
+        "value": "Bloquear"
+    },
+    "olv.portal.blocklist.add": {
+        "value": "Bloquear"
+    },
+    "olv.portal.blocklist.block_confirm": {
+        "value": "¿Estás seguro de que quieres bloquear a este usuario? Ya no recibirá solicitudes de amistad de este usuario, y será menos probable que lo encuentre en los juegos."
+    },
+    "olv.portal.blocklist.block_confirm_to.for_n3ds": {
+        "args": [1],
+        "value": "¿Desea agregar a %s en su lista de usuarios bloqueados?"
+    },
+    "olv.portal.blocklist.block_confirm_to.for_n3ds %1": {
+        "args": [1],
+        "value": "¿Desea agregar a %s en su lista de usuarios bloqueados?"
+    },
+    "olv.portal.blocklist.block_confirm_to.for_n3ds [_1]": {
+        "args": [1],
+        "value": "¿Desea agregar a %s en su lista de usuarios bloqueados?"
+    },
+    "olv.portal.blocklist.block_successed_to": {
+        "args": [1],
+        "value": "%s ha sido bloqueado."
+    },
+    "olv.portal.blocklist.block_successed_to %1": {
+        "args": [1],
+        "value": "%s ha sido bloqueado."
+    },
+    "olv.portal.blocklist.block_successed_to [_1]": {
+        "args": [1],
+        "value": "%s ha sido bloqueado."
+    },
+    "olv.portal.blocklist.delete": {
+        "value": "Desbloquear"
+    },
+    "olv.portal.blocklist.unblock_successed_to": {
+        "args": [1],
+        "value": "%s ha sido desbloqueado."
+    },
+    "olv.portal.blocklist.unblock_successed_to %1": {
+        "args": [1],
+        "value": "%s ha sido desbloqueado."
+    },
+    "olv.portal.blocklist.unblock_successed_to [_1]": {
+        "args": [1],
+        "value": "%s ha sido desbloqueado."
+    },
+    "olv.portal.button.remove": {
+        "value": "Si"
+    },
+    "olv.portal.cancel": {
+        "value": "Cancelar"
+    },
+    "olv.portal.close": {
+        "value": "Cerrar"
+    },
+    "olv.portal.confirm.remove_from_blocklist": {
+        "value": "¿Eliminar a este usuario de su lista de usuarios bloqueados?"
+    },
+    "olv.portal.confirm.remove_from_blocklist_to": {
+        "args": [1],
+        "value": "¿Quieres desbloquear a\n%s?"
+    },
+    "olv.portal.confirm.remove_from_blocklist_to %1": {
+        "args": [1],
+        "value": "¿Quieres desbloquear a\n%s?"
+    },
+    "olv.portal.confirm.remove_from_blocklist_to [_1]": {
+        "args": [1],
+        "value": "¿Quieres desbloquear a\n%s?"
+    },
+    "olv.portal.confirm_app_jump": {
+        "args": [],
+        "value": "Esto cerrará rverse.\n¿Está seguro de que desea continuar?"
+    },
+    "olv.portal.confirm_app_jump %1": {
+        "args": [],
+        "value": "Esto cerrará rverse.\n¿Está seguro de que desea continuar?"
+    },
+    "olv.portal.confirm_app_jump [_1]": {
+        "args": [],
+        "value": "Esto cerrará rverse.\n¿Está seguro de que desea continuar?"
+    },
+    "olv.portal.confirm_display_played_mark": {
+        "value": "Si has jugado al juego sobre el que estás publicando, tus publicaciones y comentarios tendrán una marca que lo indica."
+    },
+    "olv.portal.confirm_first_favorite": {
+        "value": "Añadido a comunidades favoritas."
+    },
+    "olv.portal.confirm_open_eshop": {
+        "value": "Esto cerrará rverse.\n¿Está seguro de que desea continuar?"
+    },
+    "olv.portal.confirm_syste_update.body": {
+        "value": "Debe conectarse a Internet y realizar una actualización del sistema antes de poder usar esta función. \n¿Realizar una actualización del sistema ahora?"
+    },
+    "olv.portal.confirm_syste_update.title": {
+        "value": "Por favor, realice una actualización del sistema"
+    },
+    "olv.portal.confirm_url_form.body": {
+        "value": "Esta página web se mostrará en el navegador de Internet.\n\n¿Desea cerrar rverse y ver esta página web?"
+    },
+    "olv.portal.confirm_url_form.body.for_n3ds": {
+        "value": "Esta página web se mostrará en el navegador de Internet.\n\n¿Desea cerrar rverse y ver esta página web?"
+    },
+    "olv.portal.confirm_url_form.title": {
+        "value": "Abrir enlace"
+    },
+    "olv.portal.confirm_you_have_no_soft": {
+        "value": "No tienes este software."
+    },
+    "olv.portal.continue_miiverse": {
+        "value": "Continuar usando rverse"
+    },
+    "olv.portal.dialog.apply_settings_done": {
+        "value": "Configuración guardada."
+    },
+    "olv.portal.edit.action.close_topic_post": {
+        "value": "Cerrar los comentarios"
+    },
+    "olv.portal.edit.action.close_topic_post.confirm": {
+        "value": "Ya no será posible publicar comentarios sobre esta discusión. ¿Esto esta correcto? (Esta acción no se puede revertir)."
+    },
+    "olv.portal.edit.edit_post": {
+        "value": "Editar Post"
+    },
+    "olv.portal.edit.edit_reply": {
+        "value": "Editar comentario"
+    },
+    "olv.portal.empathy.n_added": {
+        "args": [1],
+        "args_1": [1],
+        "quanttype": "1_o",
+        "value": "%s personas le dieron un like.",
+        "value_1": "%s persona le dio un like."
+    },
+    "olv.portal.empathy.n_added %1": {
+        "args": [1],
+        "args_1": [1],
+        "quanttype": "1_o",
+        "value": "%s personas le dieron un like.",
+        "value_1": "%s persona le dio un like."
+    },
+    "olv.portal.empathy.n_added [_1]": {
+        "args": [1],
+        "args_1": [1],
+        "quanttype": "1_o",
+        "value": "%s personas le dieron un like.",
+        "value_1": "%s persona le dio un like."
+    },
+    "olv.portal.empathy.you_added": {
+        "value": "Le diste un like."
+    },
+    "olv.portal.empathy.you_and_n_added": {
+        "args": [1],
+        "args_1": [1],
+        "quanttype": "1_o",
+        "value": "Tú y %s personas le dieron un like.",
+        "value_1": "Tú y %s persona le disteis un like."
+    },
+    "olv.portal.empathy.you_and_n_added %1": {
+        "args": [1],
+        "args_1": [1],
+        "quanttype": "1_o",
+        "value": "Tú y %s personas le dieron un like.",
+        "value_1": "Tú y %s persona le disteis un like."
+    },
+    "olv.portal.empathy.you_and_n_added [_1]": {
+        "args": [1],
+        "args_1": [1],
+        "quanttype": "1_o",
+        "value": "Tú y %s personas le dieron un like.",
+        "value_1": "Tú y %s persona le disteis un like."
+    },
+    "olv.portal.error.500.for_n3ds": {
+        "value": "Se produjo un error.\n\nEspere e intentelo de nuevo más tarde.\n\nSi el error continúa ocurriendo, tome nota del código de error y visite support.nintendo.com."
+    },
+    "olv.portal.error.503.content": {
+        "value": "El servidor está actualmente\nen mantenimiento programado.\n\nInténtalo de nuevo más tarde."
+    },
+    "olv.portal.error.album_limit_exceeded": {
+        "value": "No se puede guardar porque se ha alcanzado el número máximo de capturas de pantalla que se pueden guardar. Elimine algunas capturas de pantalla guardadas e intentelo de nuevo."
+    },
+    "olv.portal.error.code": {
+        "args": [1],
+        "value": "Error código: %s"
+    },
+    "olv.portal.error.code %1": {
+        "args": [1],
+        "value": "Error código: %s"
+    },
+    "olv.portal.error.code [_1]": {
+        "args": [1],
+        "value": "Error código: %s"
+    },
+    "olv.portal.error.daily_post_limit_exceeded": {
+        "value": "Has excedido la cantidad de publicaciones que puedes contribuir en un solo día. Por favor intentelo mañana de nuevo."
+    },
+    "olv.portal.error.failed_to_connect": {
+        "value": "Se ha producido un error."
+    },
+    "olv.portal.error.network_unavailable": {
+        "value": "No se pudo conectar a Internet.\nCompruebe su conexión\nred e intentelo nuevamente."
+    },
+    "olv.portal.error.post_time_restriction": {
+        "args": [],
+        "value": "No se pueden realizar múltiples publicaciones en tan poco tiempo. Intenta publicar de nuevo más tarde."
+    },
+    "olv.portal.error.post_time_restriction %1": {
+        "args": [],
+        "value": "No se pueden realizar múltiples publicaciones en tan poco tiempo. Intenta publicar de nuevo más tarde."
+    },
+    "olv.portal.error.post_time_restriction [_1]": {
+        "args": [],
+        "value": "No se pueden realizar múltiples publicaciones en tan poco tiempo. Intenta publicar de nuevo más tarde."
+    },
+    "olv.portal.followlist.confirm_unfollow_to": {
+        "args": [1],
+        "value": "¿Dejar de seguir a\n%s?"
+    },
+    "olv.portal.followlist.confirm_unfollow_to %1": {
+        "args": [1],
+        "value": "¿Dejar de seguir a\n%s?"
+    },
+    "olv.portal.followlist.confirm_unfollow_to [_1]": {
+        "args": [1],
+        "value": "¿Dejar de seguir a\n%s?"
+    },
+    "olv.portal.guest.alert.message": {
+        "value": "Se requiere una identificación de Nintendo Network para usar esta función. ¿Quieres vincular una ID de Nintendo Network?"
+    },
+    "olv.portal.guest.alert.reight": {
+        "value": "Enlace"
+    },
+    "olv.portal.miitoo.frustrated": {
+        "value": "Like..."
+    },
+    "olv.portal.miitoo.frustrated.delete": {
+        "value": "Dislike"
+    },
+    "olv.portal.miitoo.happy": {
+        "value": "¡Like!"
+    },
+    "olv.portal.miitoo.happy.delete": {
+        "value": "Dislike"
+    },
+    "olv.portal.miitoo.like": {
+        "value": "Like\u2665"
+    },
+    "olv.portal.miitoo.like.delete": {
+        "value": "Dislike"
+    },
+    "olv.portal.miitoo.normal": {
+        "value": "¡Like!"
+    },
+    "olv.portal.miitoo.normal.delete": {
+        "value": "Dislike"
+    },
+    "olv.portal.miitoo.puzzled": {
+        "value": "Like..."
+    },
+    "olv.portal.miitoo.puzzled.delete": {
+        "value": "Dislike"
+    },
+    "olv.portal.miitoo.surprised": {
+        "value": "¿¡Like!?"
+    },
+    "olv.portal.miitoo.surprised.delete": {
+        "value": "Dislike"
+    },
+    "olv.portal.my_page": {
+        "value": "Página de usuario"
+    },
+    "olv.portal.n3ds.ok": {
+        "value": "OK"
+    },
+    "olv.portal.n3ds.youtube.alert": {
+        "value": "Puede ver este video\nsi descarga la aplicación\nYouTube de\nnintendo eShop."
+    },
+    "olv.portal.n3ds.youtube.alert.patch": {
+        "value": "Descargue la última versión de la aplicación de YouTube."
+    },
+    "olv.portal.n3ds.youtube.jump": {
+        "value": "¿Quieres cerrar rverse e iniciar YouTube?"
+    },
+    "olv.portal.offline.post.delete": {
+        "value": "¿Quieres eliminar esta publicación?"
+    },
+    "olv.portal.offline.post_form": {
+        "value": "Crear publicación"
+    },
+    "olv.portal.offline.post_form.alert": {
+        "value": "No puedes guardar ninguna publicación adicional. Elimine una publicación guardada anteriormente antes de comenzar una nueva."
+    },
+    "olv.portal.ok": {
+        "value": "OK"
+    },
+    "olv.portal.post.delete_confirm": {
+        "value": "¿Eliminar esta publicación?"
+    },
+    "olv.portal.post.screenshot_forbidden.for_n3ds": {
+        "value": "Esta captura de pantalla no se puede adjuntar."
+    },
+    "olv.portal.post.screenshot_required": {
+        "value": "Por favor seleccione una captura de pantalla."
+    },
+    "olv.portal.profile_post": {
+        "value": "Publicación favorita"
+    },
+    "olv.portal.profile_post.confirm_remove": {
+        "value": "¿Eliminar esta publicación de tu perfil?\nLa publicación original no se eliminará."
+    },
+    "olv.portal.profile_post.confirm_update": {
+        "value": "¿Establecer esta publicación como su favorita?\nTenga en cuenta que se reemplazará cualquier publicación favorita existente."
+    },
+    "olv.portal.profile_post.confirm_update.yes": {
+        "value": "OK"
+    },
+    "olv.portal.profile_post.done": {
+        "value": "Tu publicación favorita se ha configurado.\n¿Quieres ver tu perfil?"
+    },
+    "olv.portal.read_more_content": {
+        "value": "Leer mas"
+    },
+    "olv.portal.reply.delete_confirm": {
+        "value": "¿Eliminar este comentario?"
+    },
+    "olv.portal.setup": {
+        "value": "Configurar"
+    },
+    "olv.portal.show_more_content": {
+        "value": "Ver toda la publicación"
+    },
+    "olv.portal.stop": {
+        "value": "Cancelar"
+    },
+    "olv.portal.unfavorite_succeeded_to": {
+        "value": "Eliminado de favoritos."
+    },
+    "olv.portal.unfollow": {
+        "value": "Dejar de seguir"
+    },
+    "olv.portal.unfollow_succeeded_to": {
+        "args": [1],
+        "value": "Ya no estás siguiendo a\n%s."
+    },
+    "olv.portal.unfollow_succeeded_to %1": {
+        "args": [1],
+        "value": "Ya no estás siguiendo a\n%s."
+    },
+    "olv.portal.unfollow_succeeded_to [_1]": {
+        "args": [1],
+        "value": "Ya no estás siguiendo a\n%s."
+    },
+    "olv.portal.user.search.found_message": {
+        "args": [1],
+        "value": "Encontrado: %s"
+    },
+    "olv.portal.user.search.found_message %1": {
+        "args": [1],
+        "value": "Encontrado: %s"
+    },
+    "olv.portal.user.search.found_message [_1]": {
+        "args": [1],
+        "value": "Encontrado: %s"
+    },
+    "olv.portal.user.search.go": {
+        "value": "Ver perfil"
+    },
+    "olv.portal.user.search.not_found_message": {
+        "args": [1],
+        "value": "\"%s\" no pudo ser encontrado.\nSeleccione Reintentar búsqueda si desea volver a intentarlo."
+    },
+    "olv.portal.user.search.not_found_message %1": {
+        "args": [1],
+        "value": "\"%s\" no pudo ser encontrado.\nSeleccione Reintentar búsqueda si desea volver a intentarlo."
+    },
+    "olv.portal.user.search.not_found_message [_1]": {
+        "args": [1],
+        "value": "\"%s\" no pudo ser encontrado.\nSeleccione Reintentar búsqueda si desea volver a intentarlo."
+    },
+    "olv.portal.welcome.select_game_skill": {
+        "value": "Selecciona tu nivel de habilidad."
+    },
+    "olv.portal.welcome.select_luminous_opt_in": {
+        "value": "Por favor seleccione una opción de notificación."
+    },
+    "olv.portal.welcome.user_data": {
+        "value": "Ingrese su nombre de usuario y su ID de Nintendo Network para continuar."
+    },
+    "olv.portal.yes": {
+        "value": "Si"
+    },
+    "olv.potal.n3ds.youtube.jump": {
+        "value": "¿Quieres cerrar rverse e iniciar YouTube?"
+    },
+    "olv.welcome.check.username": {
+        "value": "Este nombre ya está en uso, inserte otro."
+    },
+    "olv.welcome.check.nnid": {
+        "value": "Esta ID de Nintendo Network ya está en uso, inserte otro."
+    },
+    "olv.welcome.check.nonnid": {
+        "value": "Este Nintendo Network ID no existe, inserte otro."
+    },
+    "rverse.debug": {
+        "args": [1],
+        "value": "DEBUG:\n%s"
+    },
+};
