78587f098c450ea9bbbc4f9de68eff3d6f86fbaf
Jao154 <59783946+Jao154@users.noreply.github.com>
2020-07-22 00:46:32 -0300

    Brazilian Language to Rverse

---
+++ resources/lang/pt-br/errors.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Translation file for errors
+ */
+
+return [
+    '403' => [
+        'title' => 'Erro',
+        'desc' => 'Essa Tela não pode ser Exibida.',
+    ],
+
+    '404' => [
+        'title' => 'Erro',
+        'desc' => 'Essa Tela não pode ser Exibida.',
+    ],
+];
