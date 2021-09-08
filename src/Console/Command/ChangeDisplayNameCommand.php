0a28e988bb6f9f3259e981139b6bb02c111635b8
Repflez <Repflez@users.noreply.github.com>
2021-09-07 22:12:55 -0600

    Add command to change display name
    This should start making fixnnids.php obsolete for some tasks.

---
+++ src/Console/Command/ChangeDisplayNameCommand.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Holds the migration repository installer command controller.
+ */
+
+namespace Miiverse\Console\Command;
+
+use Miiverse\DB;
+use Miiverse\User;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Installs the database migration repository.
+ *
+ * @author Repflez
+ */
+class ChangeDisplayNameCommand extends Command
+{
+    /**
+     * Set up the command metadata.
+     */
+    protected function configure() : void
+    {
+        $this->setName('user:changename')
+            ->addOption('uid', null, InputOption::VALUE_REQUIRED, 'User ID or Username')
+            ->addOption('new_name', null, InputArgument::IS_ARRAY | InputArgument::REQUIRED, 'New Display Name to use')
+            ->setDescription('Changes a user\'s Display Name')
+            ->setHelp('This command allows changing a user\'s Display Name.');
+    }
+
+    /**
+     * Does the repository installing.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $formatter = $this->getHelper('formatter');
+        $user = User::construct($input->getOption('uid'));
+        $newDisplayName = $input->getOption('new_name');
+        $update = [];
+
+        if ($user->id === 0) {
+            $output->writeln($formatter->formatBlock(['Error', 'User ID "' . $input->getOption('uid') . '" not found!'], 'error'));
+
+            return Command::FAILURE;
+        }
+
+        if (!$newDisplayName) {
+            $output->writeln($formatter->formatBlock(['Error', 'Please specify a new Display Name!'], 'error'));
+
+            return Command::FAILURE;
+        }
+
+        $update['display_name'] = $newDisplayName;
+        $update['display_name_clean'] = clean_string($newDisplayName, true);
+
+        $exists = DB::table('users')
+            ->where('display_name_clean', $update['display_name_clean'])
+            ->get();
+
+        if (count($exists) > 0) {
+            $output->writeln($formatter->formatBlock(['Error', 'The Display Name "' . $newDisplayName . '" is already in use!'], 'error'));
+
+            return Command::FAILURE;
+        }
+
+        DB::table('users')
+            ->where('user_id', $user->id)
+            ->update($update);
+
+        return Command::SUCCESS;
+    }
+}
