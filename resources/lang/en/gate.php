f96549d7ee05a02a5a6bd109bb50d875783b34c3
Repflez <Repflez@users.noreply.github.com>
2021-09-10 05:29:08 -0600

    Some new setup text

--- resources/lang/en/gate.php
+++ resources/lang/en/gate.php
@@ -17,14 +17,14 @@ return [
         'about' => [
             'title' => 'About :name',
             'message1' => ':name is a recreation of Miiverse made by fans for fans.',
-            'message2' => 'Since this is a recreation, it may not have the same features as the defunct Miiverse and we\'re working on making them available as soon as possible.',
+            'message2' => 'It may not have all the features you know and love, but it can still be used nonetheless.',
         ],
         'config' => [
             'title' => 'Configuring your :name Account',
             'message' => 'We need some data from you before you can start using :name.',
             'display_name' => 'Display Name',
             'nnid' => 'Nintendo Network ID',
-            'nnid_label' => 'We use your Nintendo Network ID to fetch your Mii image.',
+            'nnid_label' => 'We use your NNID to fetch your Mii image.',
         ],
         'finish' => [
             'title' => 'Have fun in :name!',
@@ -33,8 +33,8 @@ return [
     ],
 
     'welcome_guest' => [
-        'title' => ':name is a gaming community that connects people from all over the world using Mii characters.<br>Use :name to share your gaming experiences and meet people from around the world.',
-        'message' => 'You will need a Nintendo Network ID in order to create posts or create a profile. Select Link ID to link a new or existing ID.',
+        'title' => ':name is a cgaming community that connects people from all over the world.<br>Use :name to share your gaming experiences and meet people from around the world.',
+        'message' => 'You need a Network ID in order to use :name. Select Link ID to link a new or existing ID.',
         'link' => 'Link ID',
         'exit' => 'Exit',
     ],
