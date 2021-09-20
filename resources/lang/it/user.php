77d72f4a246ba6f4ae1ad0812c71a197f0fe30b5
dixy52-beep <71886581+dixy52-beep@users.noreply.github.com>
2021-09-20 02:25:37 +0200

    Create user.php

---
+++ resources/lang/it/user.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Translation file for user
+ */
+
+return [
+    'profile' => [
+        'header' => [
+            'name' => 'Profilo di :name\',
+        ],
+
+        'meta' => [
+            // Reports
+            'violation' => [
+                'select' => 'Perfavore esegui una selezione.',
+                'create' => 'Segnala',
+                'blacklist' => 'Blocca',
+            ],
+
+            // Follow Button
+            'follow' => 'Segui',
+            'unfollow' => 'Smetti di seguire',
+
+            // Own buttons
+            'mymenu' => 'Menu Utente',
+            'settings' => 'Impostazioni del Profilo',
+
+            // To top
+            'back' => 'In cima',
+        ],
+
+        // Navigation
+        'nav' => [
+            'posts' => 'Post',
+            'empathies' => 'Yeah',
+            'following' => 'Seguiti',
+            'followers' => 'Followers',
+        ],
+
+        // Content
+        'content' => [
+            'favorite' => 'Comunità preferite',
+        ],
+
+        // Errors
+        'error' => [
+            'noposts' => 'Questo utente non ha ancora contenuti.',
+            'nofavorites' => 'Questo utente non ha ancora comunità preferite.',
+        ],
+    ],
+
+    // My Menu
+    'menu' => [
+        'header' => [
+            'desc' => 'Qui puoi modificare varie impostazioni, cercare altri utenti e altro.',
+            'title' => 'Menu Utente',
+        ],
+
+        'buttons' => [
+            'settings' => ':service Impostazioni',
+            'blacklist' => 'Utenti Bloccati',
+            'announcements' => ':service Annunci',
+            'help' => 'Manual/:service Codice di Condotta',
+        ],
+    ],
+];
