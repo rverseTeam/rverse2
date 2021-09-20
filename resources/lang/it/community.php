c2690205e9b5c1cf0bd25923cb945cdd4165ac50
dixy52-beep <71886581+dixy52-beep@users.noreply.github.com>
2021-09-20 02:06:03 +0200

    Create community.php

---
+++ resources/lang/it/community.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * Translation file for community
+ */
+
+return [
+    'index' => [
+        'header' => [
+            'desc' => 'Dai uno sguardo alle community dei giochi che ti piacciono o che ti incuriosiscono!',
+            'title' => 'Comunità',
+        ],
+        'favorites' => 'Comunità preferite',
+        'search' => [
+            'desc' => 'Cerca Comunità',
+            'name' => 'Cerca',
+        ],
+        'communities' => [
+            'filter' => [
+                'name' => 'Filtra',
+                'select' => 'Per favore esegui una selezione.',
+                'options' => [
+                    'all' => 'Tutti i Software',
+                    'games' => ':Giochi per console',
+                    'vc' => 'Virtual Console',
+                    'other' => 'Altro',
+                ],
+            ],
+            'new' => 'Nuove Comunità',
+            'special' => 'Speciale',
+            'platforms' => [
+                '3ds' => 'Titoli per 3DS',
+                'wiiu' => 'Titoli per Wii U',
+                'wiiu-3ds' => 'Titoli per Wii U・Titoli per 3DS',
+                'switch' => 'Titoli per Switch',
+                'switch-wiiu' => 'Titoli per Switch・Titoli per Wii U',
+                'wii' => 'Titoli per Wii U',
+                'ds' => 'Titoli per DS',
+                'wii-ds' => 'Titoli per Wii・Titoli per DS',
+                'special' => 'Comunità speciali',
+            ],
+            'empty' => 'Non ci sono comunità in questa categoria.',
+            'more' => 'Mostra di più',
+        ],
+        'footer' => [
+            'console' => ':Comunità di console',
+        ],
+    ],
+
+    'listing' => [
+        'filter' => [
+           'name' => 'Filtra',
+                'select' => 'Per favore esegui una selezione.',
+                'options' => [
+                    'all' => 'Tutti i Software',
+                    'games' => ':Giochi per console',
+                    'vc' => 'Virtual Console',
+                    'other' => 'Altro',
+            ],
+        ],
+        'communities' => [
+            'platforms' => [
+                 '3ds' => 'Titoli per 3DS',
+                'wiiu' => 'Titoli per Wii U',
+                'wiiu-3ds' => 'Titoli per Wii U・Titoli per 3DS',
+                'switch' => 'Titoli per Switch',
+                'switch-wiiu' => 'Titoli per Switch・Titoli per Wii U',
+                'wii' => 'Titoli per Wii U',
+                'ds' => 'Titoli per DS',
+                'wii-ds' => 'Titoli per Wii・Titoli per DS',
+            ],
+            'empty' => 'Non ci sono comunità in questa categoria.',
+        ],
+    ],
+
+    'view' => [
+        'kind' => [
+            'new' => 'Tutti',
+            'hot' => 'Popolari',
+        ],
