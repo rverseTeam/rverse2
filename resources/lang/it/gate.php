57e3e7c915deef0d77a3c7317ede10b9ea376299
dixy52-beep <71886581+dixy52-beep@users.noreply.github.com>
2021-09-20 02:15:57 +0200

    Create gate.php

---
+++ resources/lang/it/gate.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Translation file for gate
+ */
+
+return [
+    'welcome' => [
+        'controls' => [
+            'back' => 'Indietro',
+            'close' => 'Chiudi',
+            'next' => 'Avanti',
+        ],
+        'intro' => [
+            'title' => 'Benvenuto a :name!',
+            'message' => ':name ti permette di condividere la tua esperienza di gioco con il resto del mondo!.',
+        ],
+        'about' => [
+            'title' => 'Riguardo :name',
+            'message1' => ':name è un clone di Miiverse creato dai fan per i fan.',
+            'message2' => 'Potrebbero mancare alcune funzioni che conosci e ami, ma puoi usarlo comunque.',
+        ],
+        'config' => [
+            'title' => 'Configurando il tuo :name Account',
+            'message' => 'Ci servono alcuni tuoi dati prima di usare :name.',
+            'display_name' => 'Mostra il nome',
+            'nnid' => 'Nintendo Network ID',
+            'nnid_label' => 'Usiamo il tuo NNID per ottenere l'immagine del tuo Mii.',
+        ],
+        'finish' => [
+            'title' => 'Divertiti con :name!',
+            'start' => 'Inizia',
+        ],
+    ],
+
+    'welcome_guest' => [
+        'title' => ':name  una comunità di videogiocatori che connette persone da tutto il mondo.<br>Usa :name percondividere la tua esperienza di gioco con il resto del mondo.',
+        'message' => 'Ci serve il tuo Network ID per poter usare :name. Seleziona Connetti ID per connetterti a un nuovo ID o uno esistente.',
+        'link' => 'Connetti ID',
+        'exit' => 'Esci',
+    ],
+];
