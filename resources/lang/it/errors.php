e82f48cd91620440e6cc95497de54f9631b2f899
dixy52-beep <71886581+dixy52-beep@users.noreply.github.com>
2021-09-20 02:09:25 +0200

    Create errors.php

---
+++ resources/lang/it/errors.php
@@ -0,0 +1,23 @@
+  
+<?php
+/*
+ * Translation file for errors
+ */
+
+return [
+    '403' => [
+        'title' => 'Errore',
+        'desc' => 'La schermata non può essere visualizzata.',
+    ],
+
+    '404' => [
+        'title' => 'Errore',
+        'desc' => 'La schermata non può essere visualizzata.',
+    ],
+
+    'banned' => [
+        'title' => 'Account Limitato',
+        'desc' => 'Il tuo accesso a :name è stato limitato. Perfavore contatta i moderatori :name su Discord per discutere tale decisione.',
+        'button' => 'Chiudi',
+    ],
+];
