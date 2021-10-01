eb8aca6ff12e92762ffc93182d0970db85d9280e
Tescu <80772116+Tescu48@users.noreply.github.com>
2021-10-01 20:41:57 +0300

    Update post.php

--- resources/lang/ro/post.php
+++ resources/lang/ro/post.php
@@ -1,1 +1,37 @@
+<?php
+/*
+ * Translation file for post
+ */
 
+return [
+    'status' => [
+        'spoiler' => 'Spoilere',
+    ],
+
+    'content' => [
+        'spoiler' => [
+            'desc' => 'Această postare conține spoilere.',
+            'button' => 'Vedeți Postare',
+        ],
+    ],
+
+    'meta' => [
+        'empathies' => 'Da-uri',
+        'comments' => 'Commentarii',
+    ],
+
+    'extra' => [
+        'more-comments' => 'Vedeți mai multe comentarii (:count)',
+    ],
+
+    // Update on JS as well if it's updated here
+    'feelings' => [
+        'normal' => 'Da!',
+        'happy' => 'Da!',
+        'like' => 'Da♥', // No emojis here, unicode only
+        'surprised' => 'Da!?',
+        'frustrated' => 'Da...',
+        'puzzled' => 'Da...',
+        'remove' => 'Ștergeți', // Except this one
+    ],
+];
