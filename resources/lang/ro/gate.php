b45893089d6c03e5ace32076fbb66941e1a4ee48
Tescu <80772116+Tescu48@users.noreply.github.com>
2021-10-01 20:33:42 +0300

    Update gate.php

--- resources/lang/ro/gate.php
+++ resources/lang/ro/gate.php
@@ -1,1 +1,42 @@
 
+<?php
+/*
+ * Translation file for gate
+ */
+
+return [
+    'welcome' => [
+        'controls' => [
+            'back' => 'Înapoi',
+            'close' => 'Închideți',
+            'next' => 'Urm.',
+        ],
+        'intro' => [
+            'title' => 'Bine ați venit la :name!',
+            'message' => ':name vă lasă să trimiteți experiențele voastre din jocuri cu oameni din toată lumea.',
+        ],
+        'about' => [
+            'title' => 'Despre :name',
+            'message1' => ':name este o recreere a Miiverse făcut de fani pentru fani.',
+            'message2' => 'Nu are toate caracteristicile pe care le știați, dar tot poate fi folosit.',
+        ],
+        'config' => [
+            'title' 'Configurarea contului dvs. :name',
+            'message' => 'Ne trebuie câteva date despre dvs. pentru a putea începe să folosiți :name.',
+            'display_name' => 'Afisați nume',
+            'nnid' => 'Nintendo Network ID',
+            'nnid_label' => 'Vom folosi NNID-ul dvs. pentru a obține imaginea Mii-ului.',
+        ],
+        'finish' => [
+            'title' => 'Distrați-vă în :name!',
+            'start' => 'Începeți',
+        ],
+    ],
+
+    'welcome_guest' => [
+        'title' => ':name este o comunitate de gaming care conectează oameni din toată lumea.<br>Folosiți :name pentru a împărtăși experiențele dvs. de gaming și pentru a întâlni oameni din toată lumea.',
+        'message' => 'Vă trebuie un Network ID pentru a folosi :name. Selectați Link ID pentru a conecta un ID nou sau existent.',
+        'link' => 'Link ID',
+        'exit' => 'Iesiți',
+    ],
+];
