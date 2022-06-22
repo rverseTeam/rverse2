97b738d8c3cbaf4c299e4631d573555d40d382fd
Giovanni <at0mdud3@gmail.com>
2022-06-22 08:57:56 +0200

    Added Italian translations

---
+++ resources/lang/it/community.php
@@ -0,0 +1,93 @@
+<?php
+/*
+ * Translation file for community
+ */
+
+return [
+    'index' => [
+        'header' => [
+            'desc' => 'Dai un\'occhiata alle comunità sui videogiochi a cui giochi o di cui sei curioso!',
+            'title' => 'Comunità',
+        ],
+        'favorites' => 'Comunità preferite',
+        'search' => [
+            'desc' => 'Cerca comunità',
+            'name' => 'Cerca',
+        ],
+        'communities' => [
+            'filter' => [
+                'name' => 'Filtro',
+                'select' => 'Scegli una categoria.',
+                'options' => [
+                    'all' => 'Tutti i software',
+                    'games' => 'Giochi per :console',
+                    'vc' => 'Virtual Console',
+                    'other' => 'Altri',
+                ],
+            ],
+            'new' => 'Nuove comunità',
+            'special' => 'Speciali',
+            'platforms' => [
+                '3ds' => 'Giochi 3DS',
+                'wiiu' => 'Giochi Wii U',
+                'wiiu-3ds' => 'Giochi Wii U・Giochi 3DS',
+                'switch' => 'Giochi Switch',
+                'switch-wiiu' => 'Giochi Switch・Giochi Wii U',
+                'wii' => 'Giochi Wii',
+                'ds' => 'Giochi DS',
+                'wii-ds' => 'Giochi Wii・Giochi DS',
+                'special' => 'Comunità Speciali',
+            ],
+            'empty' => 'Non ci sono comunità in questa categoria.',
+            'more' => 'Vedi di piuù',
+        ],
+        'footer' => [
+            'console' => 'Comunità :console',
+        ],
+    ],
+
+    'listing' => [
+        'filter' => [
+            'name' => 'Filtro',
+            'select' => 'Scegli una categoria.',
+            'options' => [
+                'all' => 'Tutti i software',
+                'games' => 'Giochi per :console',
+                'vc' => 'Virtual Console',
+                'other' => 'Altri',
+            ],
+        ],
+        'communities' => [
+            'platforms' => [
+                '3ds' => 'Giochi 3DS',
+                'wiiu' => 'Giochi Wii U',
+                'wiiu-3ds' => 'Giochi Wii U・Giochi 3DS',
+                'switch' => 'Giochi Switch',
+                'switch-wiiu' => 'Giochi Switch・Giochi Wii U',
+                'wii' => 'Giochi Wii',
+                'ds' => 'Giochi DS',
+                'wii-ds' => 'Giochi Wii・Giochi DS',
+            ],
+            'empty' => 'Non ci sono comunità in questa categoria..',
+        ],
+    ],
+
+    'view' => [
+        'kind' => [
+            'new' => 'Tutti',
+            'hot' => 'Popolari',
+        ],
+
+        'actions' => [
+            'post' => 'Post',
+            'memo' => 'Post salvati',
+            'next' => 'Avanti',
+        ],
+    ],
+
+    'favorites' => [
+        'favorites_label' => 'Preferiti',
+        'software_used' => 'Software Usato',
+        
+    ],
+];
