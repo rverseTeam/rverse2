b00565fbc6769b41a562492f3691dc72079e11b0
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-07-05 11:31:02 +0200

    Update errors.php
    Signed-off-by: cedkeChat <123301168+00cedke@users.noreply.github.com>

--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -13,4 +13,16 @@ return [
         'title' => 'Erreur',
         'desc' => 'La page n\'a pas pu être affichée',
     ],
+
+    'banned' => [
+        'title' => 'Compte restreint',
+        'desc' => 'Votre accès à :name a été restreint. S\'il vous plaît contacter les Modérateurs :name sur Discord pour discuter de votre appel.',
+        'button' => 'Fermer',
+    ],
+
+    'pretendo' => [
+        'title' => 'Erreur d\'authentification',
+        'desc' => ':name ne peut pas authentifier votre console car vous utilisez sans doute Pretendo Network pour accéder à :name. Veuillez revenir avec Nintendo Network et réessayer.',
+        'button' => 'Fermer',
+    ],
 ];
